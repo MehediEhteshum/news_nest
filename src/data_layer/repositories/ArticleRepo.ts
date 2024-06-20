c19580c388a0f9fa45a330a31f49d9815e86be76
Mehedi <me69oshan@gmail.com>
2024-06-20 21:48:35 +0600

    news api data repo implemented

--- src/data_layer/repositories/ArticleRepo.ts
+++ src/data_layer/repositories/ArticleRepo.ts
@@ -1,20 +1,38 @@
-import { ArticleEntity } from "../../domain_layer/entities/ArticleEntity";
+import { INewsApiService } from "./../data_sources/api_ops/INewsApiService";
 import { IArticleRepo } from "../../domain_layer/repository_interfaces/IArticleRepo";
+import { ArticleModel } from "../models/ArticleModel";
+import { ApiData } from "../models/types";
 
 export class ArticleRepo implements IArticleRepo {
+  private _newsApiService: INewsApiService;
+
+  constructor(newsApiService: INewsApiService) {
+    this._newsApiService = newsApiService;
+  }
+
   // Source API
-  getRemoteNewsArticles(): Promise<ArticleEntity[]> {
-    throw new Error("Method not implemented.");
+  async getRemoteNewsArticles(): Promise<ArticleModel[]> {
+    try {
+      const newsApiData: ApiData =
+        await this._newsApiService.getRemoteNewsApiData();
+      const articles: ArticleModel[] = newsApiData.articles.map((article) => {
+        return new ArticleModel(article);
+      });
+      return articles;
+    } catch (error) {
+      console.error("ArticleRepo Error:" + error);
+      throw error;
+    }
   }
 
   // DB operations
-  getSavedNewsArticles(): Promise<ArticleEntity[]> {
+  getSavedNewsArticles(): Promise<ArticleModel[]> {
     throw new Error("Method not implemented.");
   }
-  getSavedNewsArticleById(id: string): Promise<ArticleEntity | null> {
+  getSavedNewsArticleById(id: string): Promise<ArticleModel | null> {
     throw new Error("Method not implemented. " + id);
   }
-  saveNewsArticle(article: ArticleEntity): Promise<void> {
+  saveNewsArticle(article: ArticleModel): Promise<void> {
     throw new Error("Method not implemented. " + article);
   }
   deleteNewsArticleById(id: string): Promise<void> {
