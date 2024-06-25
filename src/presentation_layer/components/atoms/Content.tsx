0c0b2e6d061c589384dd2131fd1fc9c895c53f45
Mehedi <me69oshan@gmail.com>
2024-06-25 23:35:22 +0600

    more card updates

--- src/presentation_layer/components/atoms/Content.tsx
+++ src/presentation_layer/components/atoms/Content.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren } from "react";
 
 const Content: React.FC<PropsWithChildren> = ({ children }) => {
-  return <div className="p-4">{children}</div>;
+  return <div className="p-4 pb-16">{children}</div>;
 };
 
 export default Content;
--- src/presentation_layer/components/molecules/ArticleCard.tsx
+++ src/presentation_layer/components/molecules/ArticleCard.tsx
@@ -3,30 +3,24 @@ import {
   Card,
   CardDescription,
   CardFooter,
-  CardHeader,
   CardTitle,
 } from "../shadcn/ui/card";
 import { ArticleEntity } from "@/domain_layer/entities/ArticleEntity";
 
 const ArticleCard: React.FC<ArticleEntity> = (article) => {
   return (
-    <Card
-      className="relative overflow-visible"
-      style={{ borderRadius: "1.25rem" }}
-    >
+    <Card className="relative overflow-visible rounded-2xl">
       <div className="aspect-[16/9]">
-        <img
-          src="/images/logo.png"
-          alt="Image"
-          className="object-cover opacity-30"
-        />
+        <img src="/images/logo.png" alt="Image" className="object-cover" />
       </div>
-      <div className="absolute inset-0 flex flex-col">
-        <CardHeader>
-          <CardTitle className="text-md">{article.title}</CardTitle>
-          <CardDescription>{article.author}</CardDescription>
-        </CardHeader>
-        <CardFooter>{article.isFree ? "Free" : "Premium"}</CardFooter>
+      <div className="absolute inset-0 flex flex-col top-1/2 bg-slate-900 bg-opacity-65 pt-1 px-2 rounded-b-[inherit]">
+        <CardTitle className="text-sm">
+          {article.title.substring(0, 75)}...
+        </CardTitle>
+        <CardDescription>{article.author}</CardDescription>
+        <CardFooter className="absolute p-0 pb-2 bottom-0">
+          {article.isFree ? "Free" : "Premium"}
+        </CardFooter>
       </div>
     </Card>
   );
